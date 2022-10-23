36aa3c6e2afb3c5610c78802c8f3183c6aaee00a
DinaLo <dinalo.brady@gmail.com>
2022-10-23 15:00:52 -0600

    created the User model, schema, virtual and exported it

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const usersSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        /*The source to obtain this regex was the README file with the instructions for challenge 17 */
+        match: [`/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/`]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+usersSchema
+    .virtual('friendCount').get(function () {
+        return this.friends.length
+    }
+    );
+
+const User = model('user', usersSchema);
+
+module.exports = User;
+
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Thought = require('./Thought');
+
+//Calling the Users and Thoughts models
+module.exports = { User, Thought };
